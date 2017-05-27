e7e13c75045a282ade0711cf28aae297d3f3f7e3
TrifonEstov <don.tito.g@gmail.com>
2017-05-27 10:55:55 +0300

    update

---
+++ SecondChance/1.BasicSyntaxEx05CharacterStats/CharacterStats.cs
@@ -0,0 +1,22 @@
+﻿namespace _1.BasicSyntaxEx05CharacterStats
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class CharacterStats
+    {
+        public static void Main()
+        {
+            var name = Console.ReadLine();
+            var currentHealth = int.Parse(Console.ReadLine());
+            var maxHealth = int.Parse(Console.ReadLine());
+            var currentEnergy = int.Parse(Console.ReadLine());
+            var maxEnergy = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"Name: {name}");
+            Console.WriteLine($"Health: |{new string('|', currentHealth)}{new string('.', maxHealth - currentHealth)}|");
+            Console.WriteLine($"Energy: |{new string('|', currentEnergy)}{new string('.', maxEnergy - currentEnergy)}|");
+        }
+    }
+}
