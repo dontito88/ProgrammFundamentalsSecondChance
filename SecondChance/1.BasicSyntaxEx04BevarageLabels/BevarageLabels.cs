7c95a74108424f6f7429c15229a5bd96d206b654
TrifonEstov <don.tito.g@gmail.com>
2017-05-27 10:45:39 +0300

    update

---
+++ SecondChance/1.BasicSyntaxEx04BevarageLabels/BevarageLabels.cs
@@ -0,0 +1,19 @@
+﻿namespace _1.BasicSyntaxEx04BevarageLabels
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class BevarageLabels
+    {
+        public static void Main()
+        {
+            var food = Console.ReadLine();
+            var volume = double.Parse(Console.ReadLine());
+            var energy = double.Parse(Console.ReadLine());
+            var sugar = double.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{volume}ml {food}:\n{(energy / 100) * volume}kcal, {(sugar / 100) * volume}g sugars");
+        }
+    }
+}
