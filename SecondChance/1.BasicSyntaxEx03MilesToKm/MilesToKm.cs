ec5e26131f79c1755e57245d461c8eaca528511f
TrifonEstov <don.tito.g@gmail.com>
2017-05-27 10:29:01 +0300

    update

---
+++ SecondChance/1.BasicSyntaxEx03MilesToKm/MilesToKm.cs
@@ -0,0 +1,17 @@
+﻿namespace _1.BasicSyntaxEx03MilesToKm
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class MilesToKm
+    {
+        public static void Main()
+        {
+            var kilometers = double.Parse(Console.ReadLine());
+            var miles = kilometers * 1.60934;
+
+            Console.WriteLine(miles.ToString("0.00"));
+        }
+    }
+}
