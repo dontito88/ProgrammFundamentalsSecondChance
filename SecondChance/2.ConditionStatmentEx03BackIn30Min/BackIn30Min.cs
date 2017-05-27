1f5ceae31ccab4c47570b2c0fd27144a9fd1e08d
TrifonEstov <don.tito.g@gmail.com>
2017-05-27 11:34:09 +0300

    update

---
+++ SecondChance/2.ConditionStatmentEx03BackIn30Min/BackIn30Min.cs
@@ -0,0 +1,33 @@
+﻿namespace _2.ConditionStatmentEx03BackIn30Min
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class BackIn30Min
+    {
+        public static void Main()
+        {
+            var hour = int.Parse(Console.ReadLine());
+            var min = int.Parse(Console.ReadLine());
+
+            var newMin = min + 30;
+
+            if (newMin > 59)
+            {
+                hour++;
+
+                if (hour > 23)
+                {
+                    hour = hour - 24;
+                }
+                min = newMin - min;
+                Console.WriteLine($"{hour}:{(newMin - 60).ToString("00")}");
+            }
+            else
+            {
+                Console.WriteLine($"{hour}:{newMin}");
+            }
+        }
+    }
+}
