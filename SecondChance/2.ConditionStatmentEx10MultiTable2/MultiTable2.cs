d57a37664ebb9dd372b8f4c5a5fe9272037d7a42
TrifonEstov <don.tito.g@gmail.com>
2017-05-28 12:14:42 +0300

    update

---
+++ SecondChance/2.ConditionStatmentEx10MultiTable2/MultiTable2.cs
@@ -0,0 +1,27 @@
+﻿namespace _2.ConditionStatmentEx10MultiTable2
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class MultiTable2
+    {
+        public static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            var startFrom = int.Parse(Console.ReadLine());
+
+            if (startFrom > 10)
+            {
+                Console.WriteLine($"{n} X {startFrom} = {n * startFrom}");
+            }
+            else
+            {
+                for (int i = startFrom; i <= 10; i++)
+                {
+                    Console.WriteLine($"{n} X {i} = {n * i}");
+                }
+            }
+        }
+    }
+}
