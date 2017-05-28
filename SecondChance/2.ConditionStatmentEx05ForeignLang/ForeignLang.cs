f7ff6b1bd310f17c566d1e044773ca45bd82340f
TrifonEstov <don.tito.g@gmail.com>
2017-05-28 10:45:45 +0300

    update

---
+++ SecondChance/2.ConditionStatmentEx05ForeignLang/ForeignLang.cs
@@ -0,0 +1,42 @@
+﻿namespace _2.ConditionStatmentEx05ForeignLang
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class ForeignLang
+    {
+        public static void Main()
+        {
+            var country = Console.ReadLine().ToLower();
+
+            switch (country)
+            {
+                case "usa":
+                    Console.WriteLine("English");
+                    break;
+
+                case "england":
+                    Console.WriteLine("English");
+                    break;
+
+                case "spain":
+                    Console.WriteLine("Spanish");
+                    break;
+
+                case "argentina ":
+                    Console.WriteLine("Spanish");
+                    break;
+
+                case "mexico":
+                    Console.WriteLine("Spanish");
+                    break;
+
+                default:
+                    Console.WriteLine("unknown");
+                    break;
+            }
+
+        }
+    }
+}
