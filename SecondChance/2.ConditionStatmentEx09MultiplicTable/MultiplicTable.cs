901e8c4c488aba78778f624a03a6a5927ba39a11
TrifonEstov <don.tito.g@gmail.com>
2017-05-28 11:51:49 +0300

    update

---
+++ SecondChance/2.ConditionStatmentEx09MultiplicTable/MultiplicTable.cs
@@ -0,0 +1,19 @@
+﻿namespace _2.ConditionStatmentEx09MultiplicTable
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class MultiplicTable
+    {
+        public static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"{n} X {i} = {n * i}");
+            }
+        }
+    }
+}
