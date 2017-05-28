affec18afa82b991ec976944be1bfe120b345a2c
TrifonEstov <don.tito.g@gmail.com>
2017-05-28 12:54:58 +0300

    update

---
+++ SecondChance/2.ConditionStatmentEx12NumberChecker/NumberChecker.cs
@@ -0,0 +1,23 @@
+﻿namespace _2.ConditionStatmentEx12NumberChecker
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class NumberChecker
+    {
+        public static void Main()
+        {
+
+            try
+            {
+                var input = int.Parse(Console.ReadLine());
+                Console.WriteLine("It is a number.");
+            }
+            catch 
+            {
+                Console.WriteLine("Invalid input!");
+            }
+        }
+    }
+}
