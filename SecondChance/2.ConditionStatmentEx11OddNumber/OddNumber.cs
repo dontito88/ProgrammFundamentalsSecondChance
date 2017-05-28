529f2425b70d6dc3109dd8ef147cbb1ec9d45e08
TrifonEstov <don.tito.g@gmail.com>
2017-05-28 12:50:43 +0300

    update

---
+++ SecondChance/2.ConditionStatmentEx11OddNumber/OddNumber.cs
@@ -0,0 +1,22 @@
+﻿namespace _2.ConditionStatmentEx11OddNumber
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class OddNumber
+    {
+        public static void Main()
+        {
+            var n = Math.Abs(int.Parse(Console.ReadLine()));
+
+            while (n % 2 == 0)
+            {
+                Console.WriteLine("Please write an odd number.");
+                n = Math.Abs(int.Parse(Console.ReadLine()));
+            }
+
+            Console.WriteLine($"The number is: {n}");
+        }
+    }
+}
