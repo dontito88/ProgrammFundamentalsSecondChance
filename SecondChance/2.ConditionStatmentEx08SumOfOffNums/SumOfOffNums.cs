43e65b92f3997d595608bb428a94d7d58bb530b2
TrifonEstov <don.tito.g@gmail.com>
2017-05-28 11:45:01 +0300

    update

---
+++ SecondChance/2.ConditionStatmentEx08SumOfOffNums/SumOfOffNums.cs
@@ -0,0 +1,32 @@
+﻿namespace _2.ConditionStatmentEx08SumOfOffNums
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class SumOfOffNums
+    {
+        public static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+            var sum = 0;
+            var counter = 0;
+
+            for (int i = 1; i <= 100; i++)
+            {
+                if (counter == n)
+                {
+                    break;
+                }
+                if (i % 2 != 0)
+                {
+                    Console.WriteLine(i);
+                    sum += i;
+                    counter++;
+                }
+            }
+
+            Console.WriteLine($"Sum: {sum}");
+        }
+    }
+}
