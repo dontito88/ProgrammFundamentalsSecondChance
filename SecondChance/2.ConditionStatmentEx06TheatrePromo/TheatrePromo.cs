8071e14cc9a38c8741c1cf9efded7bfd573e3a96
TrifonEstov <don.tito.g@gmail.com>
2017-05-28 11:23:43 +0300

    update - ex06 does not work absolutely properly

---
+++ SecondChance/2.ConditionStatmentEx06TheatrePromo/TheatrePromo.cs
@@ -0,0 +1,75 @@
+﻿namespace _2.ConditionStatmentEx06TheatrePromo
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class TheatrePromo
+    {
+        public static void Main()
+        {
+            var day = Console.ReadLine().ToLower();
+            var age = int.Parse(Console.ReadLine());
+
+            if (day == "weekday")
+            {
+                if (age >= 0 && age <= 18)
+                {
+                    Console.WriteLine("12$");
+                }
+                else if (age > 18 || age <= 16)
+                {
+                    Console.WriteLine("18$");
+                }
+                else if (age > 64 && age <= 122)
+                {
+                    Console.WriteLine("12$");
+                }
+                else
+                {
+                    Console.WriteLine("Error!");
+                }
+            }
+
+            else if (day == "weekend")
+            {
+                if (age >= 0 && age <= 18)
+                {
+                    Console.WriteLine("15$");
+                }
+                else if (age > 18 && age <= 16)
+                {
+                    Console.WriteLine("20$");
+                }
+                else if (age > 64 && age <= 122)
+                {
+                    Console.WriteLine("15$");
+                }
+                else
+                {
+                    Console.WriteLine("Error!");
+                }
+            }
+
+            else if (day == "holiday")
+            {
+                if (age >= 0 && age <= 18)
+                {
+                    Console.WriteLine("5$");
+                }
+                else if (age > 18 && age <= 16)
+                {
+                    Console.WriteLine("12$");
+                }
+                else if (age > 64 && age <= 122)
+                {
+                    Console.WriteLine("10$");
+                }
+                else
+                {
+                    Console.WriteLine("Error!");
+                }
+            }
+        }
+    }
+}
