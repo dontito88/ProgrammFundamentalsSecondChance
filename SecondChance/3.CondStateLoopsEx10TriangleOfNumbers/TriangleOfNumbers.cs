428f7aff26ec30619ad9701f534f3dac9e1579e7
TrifonEstov <don.tito.g@gmail.com>
2017-06-02 23:40:36 +0300

    update

---
+++ SecondChance/3.CondStateLoopsEx10TriangleOfNumbers/TriangleOfNumbers.cs
@@ -0,0 +1,23 @@
+﻿namespace _3.CondStateLoopsEx10TriangleOfNumbers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class TriangleOfNumbers
+    {
+        public static void Main()
+        {
+            var n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(i + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
