2d4f0ba69b4bcb8cd1615eeabceda53cf75ccca8
TrifonEstov <don.tito.g@gmail.com>
2017-06-04 12:49:51 +0300

    update

--- SecondChance/8.DataTypesEx05SpecialNumbers/SpecialNumbers.cs
+++ SecondChance/8.DataTypesEx05SpecialNumbers/SpecialNumbers.cs
@@ -8,7 +8,22 @@
     {
         public static void Main()
         {
+            int n = int.Parse(Console.ReadLine());
 
+            for (int i = 1; i <= n; i++)
+            {
+                int sumOfDigits = 0;
+                int digits = i;
+
+                while (digits > 0 )
+                {
+                    sumOfDigits += digits % 10;
+                    digits = digits / 10;
+                }
+
+                bool specialNumbers = (sumOfDigits == 5) || (sumOfDigits == 7) || (sumOfDigits == 11);
+                Console.WriteLine($"{i} -> {specialNumbers}");
+            }
         }
     }
 }
