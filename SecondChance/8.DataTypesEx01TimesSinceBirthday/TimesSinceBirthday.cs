20717c8226d0ddaae0ce816b82e104c622e9196a
TrifonEstov <don.tito.g@gmail.com>
2017-06-04 10:04:39 +0300

    update

---
+++ SecondChance/8.DataTypesEx01TimesSinceBirthday/TimesSinceBirthday.cs
@@ -0,0 +1,19 @@
+﻿namespace _8.DataTypesEx01TimesSinceBirthday
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class TimesSinceBirthday
+    {
+        public static void Main()
+        {
+            decimal years = decimal.Parse(Console.ReadLine());
+            decimal days = years * 365;
+            decimal hours = days * 24;
+            decimal minutes = days * 60;
+
+            Console.WriteLine($"{years} years = {days} days = {hours} hours = {minutes} minutes.");
+        }
+    }
+}
---
+++ SecondChance/8.DataTypesEx04Transport/Transport.cs
@@ -0,0 +1,20 @@
+﻿namespace _8.DataTypesEx04Transport
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Transport
+    {
+        public static void Main()
+        {
+            int persons = int.Parse(Console.ReadLine());
+
+            int capacity = 24;
+
+            int courses = (int)Math.Ceiling((double)persons / capacity);
+
+            Console.WriteLine(courses);
+        }
+    }
+}
