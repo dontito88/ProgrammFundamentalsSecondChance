714c60811e7c849c6aadc342f0601a5bc514b696
TrifonEstov <don.tito.g@gmail.com>
2017-06-13 23:47:39 +0300

    update

---
+++ SecondChance/12ArraysEx12CountLargerNums/CountLargerNums.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12ArraysEx12CountLargerNums
+{
+    class CountLargerNums
+    {
+        static void Main(string[] args)
+        {
+            double[] arrayOfNumbers = Console.ReadLine()
+                .Split(' ')
+                .Select(double.Parse)
+                .ToArray();
+            double elementForCount = double.Parse(Console.ReadLine());
+            int counter = 0;
+
+            for (int i = 0; i < arrayOfNumbers.Length; i++)
+            {
+                if (arrayOfNumbers[i] > elementForCount)
+                {
+                    counter++;
+                }
+            }
+
+            Console.WriteLine(counter);
+        }
+    }
+}
---
+++ SecondChance/12ArraysEx13IncreasingSequence/IncreasingSequence.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _12ArraysEx13IncreasingSequence
+{
+    class IncreasingSequence
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine()
+                .Split(' ')
+                .Select(int.Parse)
+                .ToArray();
+            int minValue = int.MinValue;
+            int counter = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] > minValue)
+                {
+                    minValue = input[i];
+                }
+            }
+
+            if (minValue == input[input.Length - 1])
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+        }
+    }
+}
